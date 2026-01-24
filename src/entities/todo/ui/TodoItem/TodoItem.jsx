99a00c4c9bb3036bc4838562368c6a5d670cc5c6
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2026-01-24 14:35:52 +0300

    feat: Подсветка фрагмента поиска

--- src/entities/todo/ui/TodoItem/TodoItem.jsx
+++ src/entities/todo/ui/TodoItem/TodoItem.jsx
@@ -2,6 +2,7 @@ import {memo, useContext} from "react";
 import {TasksContext} from "@/entities/todo";
 import RouterLink from "@/shared/ui/RouterLink";
 import styles from './TodoItem.module.scss'
+import {highlightCaseInsensitive} from "@/shared/utils/highlight.js";
 
 const TodoItem = (props) => {
     const {
@@ -18,7 +19,10 @@ const TodoItem = (props) => {
         toggleTaskComplete,
         disappearingTaskId,
         appearingTaskId,
+        searchQuery,
     } = useContext(TasksContext);
+
+    const highlightedTitle = highlightCaseInsensitive(title, searchQuery);
 
     return (
         <li className={`${styles.todoItem} 
@@ -43,7 +47,7 @@ const TodoItem = (props) => {
                 {title}
             </label>
             <RouterLink to={`/tasks/${id}`} aria-label="Task detail page">
-                {title}
+                <span dangerouslySetInnerHTML={{ __html: highlightedTitle}}/>
             </RouterLink>
             <button
                 className={styles.deleteButton}
