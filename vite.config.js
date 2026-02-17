8ea9297d09e87bf7e6c4620074c27a3c4f96542a
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2026-02-18 00:36:29 +0300

    feat: подготовка к деплою на GitHub Pages(настройка конфига и npm скриптов)

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,16 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL} from 'node:url'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/",
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-  preview: {
-    port: 8080,
-    strictPort: true,
-  },
-  server: {
-    port: 8080,
-    strictPort: true,
-    host: true,
-    origin: "http://localhost:8080",
-  },
+export default defineConfig(({mode}) => {
+  const isProd = mode === 'production';
+
+  return {
+    base: isProd ? '/todolist-react/' : '/',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
+  }
 })
